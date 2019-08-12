95dbb5ae7e0dd927cf7ca2f106cb6b2dedf54331
Luis Ramirez <sdaewa22@gmail.com>
2019-08-12 23:08:04 +0100

    js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
+displayWeather(){
+    
+    iconElement.innerHTML = 
+    `<img src = "icons/${weather.iconId}.png"/> `;
+
+    tempElement.innerHTML = 
+    `${weather.temperature.value} • <span>C</span>`;
+
+    descElement.innerHTML = 
+    weather.description;
+
+    locationElement.innerHTML = 
+    `${weather.city}, ${weather.country}`;
+}
