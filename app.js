6d718ac6f94955b447c60fa19b1caa13d6f1f934
Luis Ramirez <sdaewa22@gmail.com>
2019-08-22 17:49:19 +0100

    accesing HTML elements and comment on console.log(data)

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 window.addEventListener("load", () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-
-            const api = `https://api.darksky.net/forecast/9f0cd7ea5ab0a3303c9856c5c630924d/${lat}, ${long}`;
+            
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/9f0cd7ea5ab0a3303c9856c5c630924d/${lat}, ${long}`;
 
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    console.log(data);
+                    console.log(data); //  <--getting data from location
+                    const {temperature, summary} = data.currently;
                 });
         });
     }
