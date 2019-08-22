79d3356d45463f9f8af30996424c14cda9759dab
Luis Ramirez <sdaewa22@gmail.com>
2019-08-22 16:10:19 +0100

    darksky API key added

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ window.addEventListener("load", ()  => {
     if (navigator.geolocation){
          navigator.geolocation.getCurrentPosition(position => {
            long = position.coords.longitude;
-           lat = position.coords.latitude;   
+           lat = position.coords.latitude;
+
+           constapi = "https://api.darksky.net/forecast/9f0cd7ea5ab0a3303c9856c5c630924d/37.8267,-122.4233"
          });
     }else{
          h1.textContent = "Please enable location sharing"
