537b22f67a5fa97d3153754f820509bc83e5f24f
Luis Ramirez <sdaewa22@gmail.com>
2019-08-22 17:35:19 +0100

    fixed missing ;

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-window.addEventListener("load", ()  => {
+window.addEventListener("load", () => {
     let long;
-    let lat; 
+    let lat;
 
-    if (navigator.geolocation){
-         navigator.geolocation.getCurrentPosition(position => {
-           long = position.coords.longitude;
-           lat = position.coords.latitude;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
 
-           const api = `https://api.darksky.net/forecast/9f0cd7ea5ab0a3303c9856c5c630924d/${lat},${long}`;
-         });
-    }else{
-         h1.textContent = "Please enable location sharing"
+            const api = `https://api.darksky.net/forecast/9f0cd7ea5ab0a3303c9856c5c630924d/${lat}, ${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+        });
     }
 });
